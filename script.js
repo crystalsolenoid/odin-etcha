bfa7146b9999844aab9db915ef248b30df16c94e
Quinten Konyn <qkonyn@gmail.com>
2023-07-05 18:59:31 -0700

    Allow grid size to change
    I limited the sizes to 8, 16, and 32 (with a default of 16) because the
    gridlines look nicer when changes occur as a power of two. I did not go
    to 64 because of a minor graphical bug with the inner frame curve that
    breaks immersion.

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
-let boardWidth = 16;
-let boardHeight = boardWidth;
-
 const board = document.querySelector('#board');
+const sizeButton = document.querySelector('#sizeCycle');
+let sizeIndex = 0;
+const boardSizes = [16, 32, 8];
+sizeButton.addEventListener('click', cycleBoardSize);
+updateBoardSize();
 
-function styleIfCorner(cell, i, j) {
-    switch (`${i}${j}`) {
-      case '00':
+function cycleBoardSize() {
+  sizeIndex++;
+  updateBoardSize();
+}
+
+function removeOldCells() {
+  board.replaceChildren();
+}
+
+function updateBoardSize() {
+  let length = boardSizes[sizeIndex%boardSizes.length];
+  let boardWidth = length;
+  let boardHeight = boardWidth; 
+
+  removeOldCells();
+
+  for (let i = 0; i < boardHeight; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    for (let j = 0; j < boardWidth; j++) {
+      let cell = document.createElement('span');
+      cell.classList.add('cell');
+      styleIfCorner(cell, i, j, boardWidth, boardHeight);
+      row.appendChild(cell);
+    }
+    board.appendChild(row);
+  }
+
+  const cells = document.querySelectorAll('.cell');
+  function fillCell(event) {
+    let cell = event.target;
+    cell.classList.add('full');
+  }
+  cells.forEach(cell => cell.addEventListener('mouseover', fillCell));
+}
+
+function styleIfCorner(cell, i, j, boardWidth, boardHeight) {
+    switch (`${i} ${j}`) {
+      case '0 0':
         cell.classList.add('LT', 'corner');
         break;
-      case `${boardWidth-1}0`:
+      case `${boardWidth-1} 0`:
         cell.classList.add('LB', 'corner');
         break;
-      case `0${boardHeight-1}`:
+      case `0 ${boardHeight-1}`:
         cell.classList.add('RT', 'corner');
         break;
-      case `${boardWidth-1}${boardHeight-1}`:
+      case `${boardWidth-1} ${boardHeight-1}`:
         cell.classList.add('RB', 'corner');
         break;
     }
 }
 
-for (let i = 0; i < boardHeight; i++) {
-  let row = document.createElement('div');
-  row.setAttribute('class', 'row');
-  for (let j = 0; j < boardWidth; j++) {
-    let cell = document.createElement('span');
-    cell.classList.add('cell');
-    styleIfCorner(cell, i, j);
-    row.appendChild(cell);
-  }
-  board.appendChild(row);
-}
-
-const cells = document.querySelectorAll('.cell');
-function fillCell(event) {
-  let cell = event.target;
-  cell.classList.add('full');
-}
-cells.forEach(cell => cell.addEventListener('mouseover', fillCell));
